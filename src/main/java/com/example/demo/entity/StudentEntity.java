e9b35b5e0fd3c8550b26a2369c4b8de43cf3eb46
bala-amypo <bala@amypo.in>
2025-12-17 04:34:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -8,7 +8,13 @@ public class StudentEntity{
     private String name;
     private String email;
     private float cgpa;
-
+    
+    public Studententity(Long id,String name,String email,float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cgpa = cgpa;
+    }
     public Long getId(){
         return id;
     }
